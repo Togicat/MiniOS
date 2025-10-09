05b44846d06a1aefab171e969552081b0db10e10
tenczer.antal <tenczer.antal@ssakhk.cz>
2025-10-09 22:13:50 +0200

    MiniOS

---
+++ MiniOS/Core/ProcessManager.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniOS.Core;
+
+public class ProcessManager
+{
+    //Manager of processes
+    //Creates process -> PID, Name, Status(running, waiting, terminated)
+    //list of processes -> lists active processes
+    //Ending of processes -> kills PID
+    //Cleaning of processes -> Scheduler ends processes after end of and process
+    
+    
+}
---
+++ MiniOS/Program.cs
@@ -0,0 +1,18 @@
+﻿using MiniOS.Core;
+namespace MiniOS;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        //INITIALIZE OF COMPONENTS
+        Kernel kernel = new Kernel();
+        Shell shell = new Shell();
+        
+        //BOOT
+        kernel.Boot();
+        
+        
+    }
+}
